1eae9b4c1cff6a87fef2b0574d84f4bd3ee29f2c
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2025-08-05 12:23:08 +0530

    First commit

---
+++ Mumbai_Project/src/com/tnsif/inheritance/SingleLevelInheritanceDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.inheritance;
+
+public class SingleLevelInheritanceDemo {
+	
+	public static void main(String[] args) {
+		Student s1=new Student("Gayatri","Bangalore",123456778L,63806272L,123,"TNSIF");
+		System.out.println(s1);
+	}
+
+}
